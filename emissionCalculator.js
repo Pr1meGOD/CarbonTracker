a60cd410e3a1a840c341000af8ae0f7285e10581
Pr1meGOD <cubingcosmos22@gmail.com>
2024-11-06 18:45:56 +0530

    Update emissionCalculator.js

--- emissionCalculator.js
+++ emissionCalculator.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+app.use(cors());
+
 
 // Middleware to parse JSON bodies
 app.use(express.json());
