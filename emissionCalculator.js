60d99f24f40c2186b4d21587381cbb346f9582ab
Pr1meGOD <cubingcosmos22@gmail.com>
2024-11-26 01:51:07 +0530

    Update emissionCalculator.js

--- emissionCalculator.js
+++ emissionCalculator.js
@@ -20,7 +20,7 @@ const authMiddleware = (req, res, next) => {
     }
   
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, secretKey, (err, user) => {
+    jwt.verify(token, 1234, (err, user) => {
       if (err) {
         return res.status(401).json({ error: 'Unauthorized: Invalid token.' });
       }
